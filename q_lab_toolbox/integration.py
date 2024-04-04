b829e0f469364ca49536e1030521cf6b3144d814
davidvrchen <d.v.r.chen@student.tue.nl>
2024-04-04 15:17:16 +0200

    updated documentation

--- q_lab_toolbox/integration.py
+++ q_lab_toolbox/integration.py
@@ -1,6 +1,7 @@
 """
+WIP
 Provides some function to make ts to integrate Lindbladian on,
-should have been part of solve_lindblad branch
+(should have been part of solve_lindblad branch)
 
 Info:
     Created on Wed March 13 2024
@@ -32,7 +33,7 @@ class Ts:
 @dataclass
 class BasicLinspace(Ts):
     """Use n_steps evenly spaced time steps (aka np.linspace)
-    
+
     Args:
     -----
     t_max (float): Solve Lindblad from 0 to t_max
@@ -43,20 +44,26 @@ class BasicLinspace(Ts):
     n_steps: int
 
 
+def _basic_ts(t_max, n_steps):
+    """Create ts array with n_steps from 0 to t_max."""
+    # create ts
+    return np.linspace(0, t_max, n_steps)
+
+
 def basic_ts(s: BasicLinspace):
-    """Create ts array of evenly spaced """
+    """Convenience function to create ts from BasicLinspace object."""
 
     # read parameters from settings
     t_max = s.t_max
     n_steps = s.n_steps
 
     # create ts
-    return np.linspace(0, t_max, n_steps)
+    return _basic_ts(t_max=t_max, n_steps=n_steps)
 
 
 def create_ts(s: Ts):
     """Convenience function that creates the appropriate
-    Hamiltonian from settings."""
+    ts from settings."""
 
     if isinstance(s, BasicLinspace):
         return basic_ts(s)
