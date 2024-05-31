13a19ea1899343902e32b892156563386e30ea42
davidvrchen <d.v.r.chen@student.tue.nl>
2024-05-31 12:20:15 +0200

    added example where identity hamiltonian is used

--- results/only_decay/settings.py
+++ results/only_decay/settings.py
@@ -2,7 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from q_channel_approx.physics_defns import (
-    DecaySystem,
+    NothingSystem,
+    no_decay_jump_operators,
     default_jump_operators,
     create_hamiltonian,
     rho_rand_haar,
@@ -24,16 +25,18 @@ from q_channel_approx.channel import evolver_fac
 from q_channel_approx.plotting.plot_observables import create_observables_comp_basis
 from q_channel_approx.plotting.visualize import compare_ess
 from q_channel_approx.pprint import comp_basis_labels
+from q_channel_approx.optimizer import optimize
 
 m = 1
 omegas = (0.5,)
 gammas = (0.5,)
 ryd_interaction = 1.8
 
-system = DecaySystem(ryd_interaction=ryd_interaction, omegas=omegas, m=m, gammas=gammas)
+system = NothingSystem(m=m, gammas=gammas)
 
 rho0s = random_rho0s(m=1, L=10)
 
+jump_oper_no_decay = no_decay_jump_operators()
 jump_oper = default_jump_operators(m, gammas=gammas)
 
 rhoss, ts = solve_lindblad_rho0s(rho0s=rho0s, delta_t=0.5, N=3, s=system, jump_opers=jump_oper)
